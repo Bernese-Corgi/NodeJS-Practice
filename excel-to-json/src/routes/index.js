75af828051a29290bf23aa842f1da1f55ab26905
Bernese-Corgi <jnyng0621@gmail.com>
2022-05-06 01:45:42 +0900

    [excel-to-json] feat: Add json data using sheet_to_json util function

--- excel-to-json/src/routes/index.js
+++ excel-to-json/src/routes/index.js
@@ -2,10 +2,17 @@ const express = require('express');
 const router = express.Router();
 const XLSX = require('xlsx');
 
-router.get('/excel', (req, res, mext) => {
-  let workbook = XLSX.readFile(__dirname + '/../../public/ApiDocs.xlsx');
-  let worksheet = workbook.Sheets['Sheet1'];
-  res.json(worksheet);
+router.get('/', (req, res, mext) => {
+  const excelFile = XLSX.readFile(__dirname + '/../../public/_ApiDocs.xlsx');
+  const { SheetNames, Sheets } = excelFile;
+
+  // 참조 스타일 등 셀의 모든 정보를 같이 가져온다.
+  const workSheet = Sheets[SheetNames[0]];
+  // res.send(workSheet);
+
+  // 셀의 내용만 가져온다.
+  const jsonData = XLSX.utils.sheet_to_json(workSheet);
+  res.send(jsonData);
 });
 
 module.exports = router;
