304934a8dc27645588d2444db6887dc40a8e521f
Bernese-Corgi <jnyng0621@gmail.com>
2022-05-06 00:44:10 +0900

    [excel-to-json] feat: Add router to read excel file with xlsx npm module and Remove code for excel npm module

--- excel-to-json/src/routes/index.js
+++ excel-to-json/src/routes/index.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const parseXlsx = require('excel'); //
+const XLSX = require('xlsx');
 
-// Excel 파일을 파싱하는 라우터 함수 (excel 모듈 사용)
-router.get('/excelShow', function (req, res, next) {
-  // 엑셀 파일이 있는 경로를 전달해 엑셀을 읽고, 그 데이터를 json으로 응답한다.
-  parseXlsx(__dirname + '../../public/nodeexcel.xlsx', function (err, data) {
-    res.json(data);
-  });
+router.get('/excel', (req, res, mext) => {
+  let workbook = XLSX.readFile(__dirname + '/../../public/ApiDocs.xlsx');
+  let worksheet = workbook.Sheets['Sheet1'];
+  res.json(worksheet);
 });
 
 module.exports = router;
