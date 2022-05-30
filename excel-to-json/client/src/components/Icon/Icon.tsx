e6d113dcf5fd4725eb994da78efb96e6350b5914
Bernese-Corgi <jnyng0621@gmail.com>
2022-05-30 23:27:45 +0900

    [excel-to-json] feat: Add Icon component

---
+++ excel-to-json/client/src/components/Icon/Icon.tsx
@@ -0,0 +1,70 @@
+import React, { ReactComponentElement, ReactNode } from 'react';
+
+// const Sprite = require('./sprites.svg') as ReactNode;
+
+export type IconShape =
+  | 'arrow-rotate-left-solid'
+  | 'arrow-rotate-right-solid'
+  | 'backward-step-solid'
+  | 'forward-step-solid'
+  | 'play-solid'
+  | 'pause-solid'
+  | 'volume-xmark-solid'
+  | 'volume-high-solid'
+  | 'angles-down-solid';
+
+export type IconProps = {
+  id?: IconShape;
+  title?: string;
+  shape: string;
+  color?: string;
+  size?: { width: string | number; height: string | number };
+  onClick?: () => void;
+};
+// const Svg = styled.svg<IconProps>`
+//   ${({ color, shape, onClick, size }) => css`
+//     color: ${handleColorType(color)};
+//     width: ${size?.width ? size.width : '1em'};
+//     height: ${size?.height ? size.height : '1em'};
+//     ${theme.transition()}
+
+//     &:hover {
+//       color: ${handleHoverColorType(shape, color)};
+//       cursor: ${onClick ? 'pointer' : 'initial'};
+//     }
+
+//     ${StyledIconButton}:hover & {
+//       color: ${handleHoverColorType(shape, color)};
+//       cursor: pointer;
+//     }
+//   `}
+// `;
+
+const Icon = ({
+  id,
+  title,
+  shape,
+  color,
+  size,
+  onClick,
+  ...restProps
+}: IconProps) => {
+  // console.log(Sprite);
+
+  return (
+    <>
+      <svg id={id} width="100%" height="100%" onClick={onClick}>
+        <use
+          id={id}
+          aria-label={title}
+          // xlinkHref={`${Sprite}#${shape}`}
+          xlinkHref={`./sprites.svg#${shape}`}
+          width="100%"
+          height="100%"
+        />
+      </svg>
+    </>
+  );
+};
+
+export default Icon;
