d27da824c5803dadd30a5afe9fa1b37b1078a1e2
Bernese-Corgi <jnyng0621@gmail.com>
2022-06-02 23:58:59 +0900

    [excel-to-json] feat: Add pagination in allPlaylist controller

--- excel-to-json/server/src/api/allPlaylist.js
+++ excel-to-json/server/src/api/allPlaylist.js
@@ -1,15 +1,26 @@
-const { Router } = require('express');
+const { Router, json } = require('express');
 const XLSX = require('xlsx');
 
 const allPlaylist = Router();
 
 allPlaylist.get('/', (req, res, next) => {
+  const LIMIT = 10;
+  const page = parseInt(req.query.page || 1, 10); // 값이 주어지지 않았다면 1을 사용
+
+  if (page < 1) {
+    ctx.status = 400;
+    return;
+  }
+
   const excelFile = XLSX.readFile(__dirname + '/../../public/AllPlaylist.xlsx');
   const { SheetNames, Sheets } = excelFile;
 
   const workSheet = Sheets[SheetNames[0]];
 
-  const jsonData = XLSX.utils.sheet_to_json(workSheet);
+  const jsonData = XLSX.utils
+    .sheet_to_json(workSheet)
+    .splice(LIMIT * (page - 1), LIMIT * page);
+
   res.send(jsonData);
 });
 
