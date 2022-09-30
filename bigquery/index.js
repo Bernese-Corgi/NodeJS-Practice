ab72356cb6b30bef941f4cd267bb14a45bf98e22
Bernese-Corgi <jnyng0621@gmail.com>
2022-09-30 10:02:08 +0900

    [bigquery] fix: rows 구할때 문법 수정

--- bigquery/index.js
+++ bigquery/index.js
@@ -10,6 +10,6 @@ const dataset = bigquery.dataset(process.env.DATASET_ID);
 
 (async () => {
   const table = await dataset.table(process.env.ACCOUNT_TABLE_ID)
-  const [rows] = await table.query(`SELECT name, logined_at LIMIT 10`)
+  const [rows] = await table.query(`SELECT name, logined_at FROM ${`${process.env.ACCOUNT_TABLE_ID}`} LIMIT 10`)
 
 })();
